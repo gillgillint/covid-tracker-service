0e4cc36a7c8d2a3b861b3cdc502fb73e4b33c99e
kantapong_int <kantapong.int.work@gmail.com>
2023-09-17 01:59:07 +0700

    add return

--- app.js
+++ app.js
@@ -36,19 +36,20 @@ app.get("/api", (req, res) => {
 app.get("/api/v1/covid", (req, res) => {
   try {
     const { date } = req.query;
-
+    let data;
     connection.query(
       `SELECT * from covid_countries cc 
             WHERE day= ${date}'
             ORDER BY cases DESC
             LIMIT 10`,
       (err, results, fields) => {
-        res.status(results);
+        data = results;
       }
     );
 
-    res.status(200).json({
+    return res.status(200).json({
       status: "success",
+      data,
     });
   } catch (error) {}
 });
